d3ecaca786d12ec5fb07c7d454621f7144c04010
azizul6817 <azizulhakim68178@gmail.com>
2026-01-20 06:57:42 +0600

    axiosInstance-url-changed

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://foods-sharing-server-assign-10.vercel.app",
 });
 
 export default axiosInstance;
